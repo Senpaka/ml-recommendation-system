86d6cbfedb3a41a48244d7694adb9cef912e8439
Senpaka <senpaka@yandex.ru>
2026-09-11 15:02:52 +0300

    refactor: add validation on temporal splilt

--- src/temporal_split.py
+++ src/temporal_split.py
@@ -7,30 +7,37 @@ class TemporalSplit:
     def split(
         self, 
         data: pd.DataFrame,
-        test_size: float = 0.8
-    ) -> tuple[pd.DataFrame, pd.DataFrame]:
+        train_size: float = 0.8
+    ) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
 
-        if not 0 < test_size < 1:
-            raise ValueError("test_size must be between 0 and 1")
+        if not 0 < train_size < 1:
+            raise ValueError("train_size must be between 0 and 1")
 
         train_part = []
         test_part = []
+        val_part = []
 
         df = data.copy().sort_values(by=["user_id", "timestamp"])
 
         for _, group in df.groupby("user_id"):
             
-            split_index = int(len(group) * (1 - test_size))
+            split_index = int(len(group) * train_size)
+            val_test_split_index = split_index + int(len(group) * (1 - train_size))
 
             train_part.append(
                 group.iloc[:split_index]
             )
 
+            val_part.append(
+                group.iloc[split_index:val_test_split_index]
+            )
+
             test_part.append(
-                group.iloc[split_index:]
+                group.iloc[val_test_split_index:]
             )
 
         train_df = pd.concat(train_part).reset_index(drop=True)
         test_df = pd.concat(test_part).reset_index(drop=True)
+        val_df = pd.concat(val_part).reset_index(drop=True)
     
-        return train_df, test_df
+        return train_df, val_df, test_df
